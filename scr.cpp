4d2199b006c75329bd547a5e395d8a8ae8f52e07
gorodilina <104147754+gorodilina@users.noreply.github.com>
2022-04-28 19:45:15 +0300

    Update scr.cpp

--- scr.cpp
+++ scr.cpp
@@ -6,6 +6,152 @@ double dist( pt a, pt b ){
     return sqrt( pow( a.x - b.x, 2 ) + pow( a.y - b.y, 2 ) );
 }
 
+void show_mat(vector<vector<item>> &M)
+{
+    int N=M.size();
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<M[i].size(); j++)
+        {
+            cout<< M[i][j].id<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+vector<item> knapsack (vector<item>& it, int K)
+{
+    int N=it.size();
+    vector<vector<int>> w(K+1, vector<int>(N+1));
+    vector <vector<vector<item>>> used(K+1, vector<vector<item>>(N+1));
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<K; j++)
+        {
+            if (it[i].weight<=j+1)
+            {
+                if (w[j+1][i]>=w[j+1-it[i].weight][i]+it[i].value)
+                {
+                    w[j+1][i+1]=w[j+1][i];
+                    used[j+1][i+1]=used[j+1][i];
+                }
+                else
+                {
+                    w[j+1][i+1]=w[j+1-it[i].weight][i]+it[i].value;
+                    used[j+1][i+1]=used[j+1-it[i].weight][i];
+                    used[j+1][i+1].push_back(it[i]/*.value*/);
+                }
+            }
+            else
+            {
+                w[j+1][i+1]=w[j+1][i];
+                used[j+1][i+1]=used[j+1][i];
+            }
+        }
+    }
+    return used[K][N];
+}
+
+
+vector<vector<item>> k_means(vector <item>& points, double mean)
+{
+    int N=points.size();
+    vector<vector<item>> clusters(2);
+    for (int i=0; i<N; i++)
+    {
+        if (points[i].x>mean)
+            clusters[0].push_back(points[i]);
+        else
+            clusters[1].push_back(points[i]);
+    }
+    clusters[0].push_back(points[0]);
+    clusters[1].push_back(points[0]);
+    return clusters;
+}
+
+pair<double, int> cvrp(vector<item>& pnt, int V, int c)
+{
+    //каждый грузовик воображаем рюкзаком, каждую потребность - предметом, вес равен ценности
+    vector<vector<item>>usedused;//наборы городов для каждого грузовика
+    vector<item>p=pnt;
+    double ANS=0;
+    for (int i=0; i<V; i++)
+    {
+            usedused.push_back(knapsack(p, c));
+            for (int j=0; j<usedused[i].size(); j++)
+                {
+                    for (int k=0; k<p.size(); k++)
+                    {
+                        if (usedused[i][j].id==p[k].id)
+                            p.erase(p.begin()+k);
+                    }
+                }
+            if (p.size()==0)
+                break;
+    }
+    V=V+2-usedused.size();
+    double ans=0;
+
+    for (int i=0; i<usedused.size(); i++)
+    {
+        Path cur;
+        for (int j=0; j<usedused[i].size(); j++)
+        {
+             pt newone = {usedused[i][j].id, usedused[i][j].x, usedused[i][j].y};
+   	         cur.add_node(newone);
+        }
+        cur.local_search_2();
+        ans+=cur.length();
+    }
+    ANS+=ans;
+    //cout <<endl<< "ANS="<< ans;
+    return {ANS, V};
+}
+
+/*
+double cvrp(vector<item>& points, int V, int c)
+{
+    double ANS=0;
+    vector<vector<item>>usedused;//наборы городов для каждого грузовика
+    vector<item>p=points;//clusters[i];
+    for (int i=0; i<V; i++)
+    {
+            usedused.push_back(knapsack(p, c));
+            for (int j=0; j<usedused[i].size(); j++)
+                {
+                    for (int k=0; k<p.size(); k++)
+                    {
+                        if (usedused[i][j].id==p[k].id)
+                            p.erase(p.begin()+k);
+                    }
+                }
+            if (p.size()==0)
+                break;
+    }
+    V=V+2-usedused.size();
+    double ans=0;
+
+    for (int i=0; i<usedused.size(); i++)
+    {
+        Path cur;
+        for (int j=0; j<usedused[i].size(); j++)
+        {
+             pt newone = {usedused[i][j].id, usedused[i][j].x, usedused[i][j].y};
+   	         cur.add_node(newone);
+        }
+        cur.local_search_2();
+        ans+=cur.length();
+        //cout << cur.length() << endl;
+        //cur.show();
+        //cout << cur.length() << endl;
+    }
+    ANS+=ans;
+    return ANS;
+    //cout <<endl<< "ANS="<< ANS;
+}*/
+
+
 Path::Path(){
     num_of_nodes = 0;
     last_improvement_status = false;
